0ccb60e126b01780f38b1aa6483a394070bf95c5
capricornus84 <capricornus@inbox.ru>
2022-06-30 19:57:21 +0300

    version 2.0

--- app/src/main/java/com/dvm/dvmproject8/MainActivity.kt
+++ app/src/main/java/com/dvm/dvmproject8/MainActivity.kt
@@ -2,10 +2,34 @@ package com.dvm.dvmproject8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button4 = findViewById<Button>(R.id.button4)
+        val button5 = findViewById<Button>(R.id.button5)
+        button1.setOnClickListener {
+            Toast.makeText(this, "Профиль", Toast.LENGTH_SHORT).show()
+        }
+        button2.setOnClickListener {
+            Toast.makeText(this, "@Сегодня в кино", Toast.LENGTH_SHORT).show()
+        }
+        button3.setOnClickListener {
+            Toast.makeText(this, "Буду смотреть", Toast.LENGTH_SHORT).show()
+        }
+        button4.setOnClickListener {
+            Toast.makeText(this, "Загрузки", Toast.LENGTH_SHORT).show()
+        }
+        button5.setOnClickListener {
+            Toast.makeText(this, "О приложении", Toast.LENGTH_SHORT).show()
+        }
     }
 }
