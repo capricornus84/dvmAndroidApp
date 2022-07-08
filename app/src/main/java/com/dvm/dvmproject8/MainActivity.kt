4a2bc8cd36cf6c770773f07cccf5fa6895d3f0f2
capricornus84 <capricornus@inbox.ru>
2022-07-08 18:14:47 +0300

    version 3.1

--- app/src/main/java/com/dvm/dvmproject8/MainActivity.kt
+++ app/src/main/java/com/dvm/dvmproject8/MainActivity.kt
@@ -6,35 +6,22 @@ import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Toast
+import com.dvm.dvmproject8.databinding.ActivityMainBinding
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        /*val button1 = findViewById<Button>(R.id.button1)
-        val button2 = findViewById<Button>(R.id.button2)
-        val button3 = findViewById<Button>(R.id.button3)
-        val button4 = findViewById<Button>(R.id.button4)
-        val button5 = findViewById<Button>(R.id.button5)
-        button1.setOnClickListener {
-            Toast.makeText(this, "Профиль", Toast.LENGTH_SHORT).show()
-        }
-        button2.setOnClickListener {
-            Toast.makeText(this, "@Сегодня в кино", Toast.LENGTH_SHORT).show()
-        }
-        button3.setOnClickListener {
-            Toast.makeText(this, "Буду смотреть", Toast.LENGTH_SHORT).show()
-        }
-        button4.setOnClickListener {
-            Toast.makeText(this, "Загрузки", Toast.LENGTH_SHORT).show()
-        }
-        button5.setOnClickListener {
-            Toast.makeText(this, "О приложении", Toast.LENGTH_SHORT).show()
-        }*/
-        val topAppBar = findViewById<MaterialToolbar>(R.id.settings)
-        topAppBar.setOnMenuItemClickListener {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        //setSupportActionBar(binding.topAppBar)
+
+        binding.topAppBar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.settings -> {
                     Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
@@ -43,10 +30,8 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
-        //val bottom_navigation: BottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottom_navigation.setOnNavigationItemSelectedListener {
-        //bottom_navigation.OnItemSelectedListener {
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
 
             when (it.itemId) {
                 R.id.favorites -> {
