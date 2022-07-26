27a8ebe5d6db6d093c9798d1dceebb9e92637729
capricornus84 <capricornus@inbox.ru>
2022-07-26 16:50:20 +0300

    6

--- app/src/main/java/com/dvm/dvmproject8/MainActivity.kt
+++ app/src/main/java/com/dvm/dvmproject8/MainActivity.kt
@@ -1,11 +1,83 @@
 package com.dvm.dvmproject8
 
+import android.content.Intent
+import android.content.res.Resources
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.dvm.dvmproject8.databinding.ActivityMainBinding
+import kotlinx.android.parcel.Parcelize
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //находим наш RV
+        binding.mainRecycler.apply {
+            //Инициализируем наш адаптер в конструктор передаем анонимно инициализированный интерфейс,
+            //оставим его пока пустым, он нам понадобится во второй части задания
+            filmsAdapter = FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener{
+                override fun click(film: Film) {
+                    //Создаем бандл и кладем туда объект с данными фильма
+                    val bundle = Bundle()
+                    //Первым параметром указывается ключ, по которому потом будем искать, вторым сам
+                    //передаваемый объект
+                    bundle.putParcelable("film", film)
+                    //Запускаем наше активити
+                    val intent = Intent(this@MainActivity, DetailsActivity::class.java)
+                    //Прикрепляем бандл к интенту
+                    intent.putExtras(bundle)
+                    //Запускаем активити через интент
+                    startActivity(intent)
+                }
+            })
+            //Присваиваем адаптер
+            adapter = filmsAdapter
+            //Присвои layoutmanager
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            //Применяем декоратор для отступов
+            val decorator = TopSpacingItemDecoration(8)
+            addItemDecoration(decorator)
+        }
+//Кладем нашу БД в RV
+        filmsAdapter.addItems(filmsDataBase)
+    }
+    private lateinit var filmsAdapter: FilmListRecyclerAdapter
+}
+
+@Parcelize
+data class Film(
+    val title: String,
+    val poster: Int,
+    val description: String
+) : Parcelable
+
+val filmsDataBase = listOf(
+    Film("American psycho", R.drawable.americanpsycho, "A wealthy New York City investment banking executive, Patrick Bateman, hides his alternate psychopathic ego from his co-workers and friends as he delves deeper into his violent, hedonistic fantasies."),
+    Film("Back to the Future", R.drawable.backtothefuture, "Marty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his close friend, the eccentric scientist Doc Brown."),
+    Film("Jurassic Park", R.drawable.jurassicpark, "A pragmatic paleontologist touring an almost complete theme park on an island in Central America is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose."),
+    Film("Pulp Fiction", R.drawable.pulpfiction, "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption."),
+    Film("Raiders of the lost ark", R.drawable.raidersofthelostark, "Archaeology professor Indiana Jones ventures to seize a biblical artefact known as the Ark of the Covenant. While doing so, he puts up a fight against Renee and a troop of Nazis."),
+    Film("Scream", R.drawable.scream, "A year after the murder of her mother, a teenage girl is terrorized by a new killer, who targets the girl and her friends by using horror films as part of a deadly game."),
+    Film("Star Wars", R.drawable.starwars, "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader."),
+)
+
+class TopSpacingItemDecoration (private val paddingInDp: Int): RecyclerView.ItemDecoration() {
+    private val Int.convertPx: Int
+        get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        super.getItemOffsets(outRect, view, parent, state)
+        outRect.top = paddingInDp.convertPx
+        outRect.right = paddingInDp.convertPx
+        outRect.left = paddingInDp.convertPx
+
     }
 }
