5c0c92d8252b8c1e38b7bca03886436b64d0f15e
capricornus84 <capricornus@inbox.ru>
2022-10-19 18:00:24 +0300

    13

--- app/src/main/java/com/dvm/dvmproject8/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/dvm/dvmproject8/viewmodel/HomeFragmentViewModel.kt
@@ -5,11 +5,15 @@ import androidx.lifecycle.ViewModel
 import com.dvm.dvmproject8.App
 import com.dvm.dvmproject8.domain.Film
 import com.dvm.dvmproject8.domain.Interactor
+//import org.koin.core.KoinComponent
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+//import org.koin.core.inject
 
-class HomeFragmentViewModel : ViewModel() {
+class HomeFragmentViewModel : ViewModel(), KoinComponent {
     val filmsListLiveData:  MutableLiveData<List<Film>> = MutableLiveData()
     //Инициализируем интерактор
-    private var interactor: Interactor = App.instance.interactor
+    private val interactor: Interactor by inject()
     init {
         interactor.getFilmsFromApi(1, object : ApiCallback {
             override fun onSuccess(films: List<Film>) {
