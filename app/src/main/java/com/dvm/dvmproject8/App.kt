12305d0a0ebea017febbb8678fe2fcd544229039
capricornus84 <capricornus@inbox.ru>
2023-01-12 17:32:27 +0300

    26

--- app/src/main/java/com/dvm/dvmproject8/App.kt
+++ app/src/main/java/com/dvm/dvmproject8/App.kt
@@ -1,10 +1,14 @@
 package com.dvm.dvmproject8
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import com.dvm.dvmproject8.di.AppComponent
 import com.dvm.dvmproject8.di.DaggerAppComponent
 import com.dvm.dvmproject8.di.modules.DatabaseModule
 import com.dvm.dvmproject8.di.modules.DomainModule
+import com.dvm.dvmproject8.view.notifications.NotificationConstants.CHANNEL_ID
 //import com.dvm.dvmproject8.di.modules.RemoteModule
 import com.dvm.remote_module.DaggerRemoteComponent
 
@@ -22,6 +26,21 @@ class App : Application() {
             .databaseModule(DatabaseModule())
             .domainModule(DomainModule(this))
             .build()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //Задаем имя, описание и важность канала
+            val name = "WatchLaterChannel"
+            val descriptionText = "FilmsSearch notification Channel"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            //Создаем канал, передав в параметры его ID(строка), имя(строка), важность(константа)
+            val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
+            //Отдельно задаем описание
+            mChannel.description = descriptionText
+            //Получаем доступ к менеджеру нотификаций
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            //Регистрируем канал
+            notificationManager.createNotificationChannel(mChannel)
+        }
     }
 
     companion object {
---
+++ app/src/main/java/com/dvm/dvmproject8/view/notifications/NotificationConstants.kt
@@ -0,0 +1,5 @@
+package com.dvm.dvmproject8.view.notifications
+
+object NotificationConstants {
+    const val CHANNEL_ID = "FilmsSearchChannel"
+}
